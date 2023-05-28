4754b769c7e80b7736b74616c58b82695106ee54
NakHwa <lms990108@gmail.com>
2023-05-28 22:09:18 +0900

    - 오류로 인한 프로젝트 재 생성
    1. 의존성
    
    2. application.yml 작성
    
    3. 틀이되는 패키지 생성

---
+++ src/main/java/com/example/springboardproject/SpringboardprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboardprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboardprojectApplication.class, args);
+    }
+
+}
